3f987c0025681ffef3da4f680cac2e34114215a9
meng yang <xiaoruiqiao@gmail.com>
2017-09-28 08:34:13 +0900

    add sum area function

--- Denoise/Denoise/core.h
+++ Denoise/Denoise/core.h
@@ -25,3 +25,4 @@ void shapeFilter(Mat& diff_wb, Mat& labels, Mat& stats,int size,vector<int>& val
 void distributeFilter(Mat& diff_wb, Mat& labels,Mat& stats, Mat& gray,vector<int>& valid_labels,vector<int>& valid_labels1);
 void maskRefinement(Mat& diff_wb, Mat& labels, Mat& gray, vector<int>& valid_labels2);
 void medianFramesByMask(Mat& image, Mat& stats, vector<int>& valid_labels);
+void sumAreaByRadius(vector<Mat>& diff_wb, Mat& sum, int radius);
