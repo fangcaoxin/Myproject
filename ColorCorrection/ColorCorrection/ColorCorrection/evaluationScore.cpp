0db42f58719db776d2f38dc978fed8a23d641685
xiaoruiqiao <xiaoruiqiao@gmail.com>
2017-11-07 23:15:08 +0900

    edit evaluationScore.cpp

--- ColorCorrection/ColorCorrection/ColorCorrection/evaluationScore.cpp
+++ ColorCorrection/ColorCorrection/ColorCorrection/evaluationScore.cpp
@@ -16,13 +16,14 @@ double evaluationScore(Mat& src_original) {
 	src_original.copyTo(src);
 	Mat src_lab,src_float;
 	vector<Mat> lab_channels;
-	Scalar l_mean, l_stddev,a_mean,a_stddev;
+	Scalar b_mean, b_stddev,a_mean,a_stddev;
 	if (src.type() == CV_8UC3) {
 		src.convertTo(src_float, CV_32FC3);
 		normalize(src_float, src_float, 0, 1, NORM_MINMAX, -1, Mat());
 	}
 	else if(src.type()==CV_32FC3){
 		src_float = src;
+		normalize(src_float, src_float, 0, 1, NORM_MINMAX, -1, Mat());
 	}
 	else {
 		cout << "not support " << endl;
@@ -33,8 +34,8 @@ double evaluationScore(Mat& src_original) {
 	cvtColor(src_float, src_float,CV_BGR2Lab);
 	normalize(src_float, src_float, 0, 1, NORM_MINMAX, -1, Mat());
 	split(src_float, lab_channels);
-	meanStdDev(lab_channels[0], l_mean, l_stddev);
-	meanStdDev(lab_channels[1], a_mean, a_stddev);
+	meanStdDev(lab_channels[0], a_mean, a_stddev);
+	meanStdDev(lab_channels[1], b_mean, b_stddev);
 	Mat flat;
 	vector<float> low_value, high_value;
 	lab_channels[2].reshape(1, 1).copyTo(flat);
@@ -46,7 +47,18 @@ double evaluationScore(Mat& src_original) {
 	for (int j = flat.cols - 1; j >= flat.cols - area; j--) {
 		high_value.push_back(flat.at<float>(0, j));
 	}
-	double con_l=norm(low_value, high_value)/(norm(low_value)*norm(high_value));
-	double score = c1*l_stddev[0] + c2*con_l + c3*a_mean[0];
+	double low_high_dot = 0.,high_dot=0.,low_dot=0.;
+	for (int k = 0; k < area; k++) {
+		low_high_dot += low_value[k] * high_value[k];
+		high_dot += high_value[k] * high_value[k];
+		low_dot += low_value[k] * low_value[k];
+	}
+	double low_norm = norm(low_value);
+	double high_norm = norm(high_value);
+	double low_high_norm = norm(low_value, high_value);
+	//double con_l=low_high_norm/(low_norm*high_norm);
+//double con_l = 1-low_high_dot / sqrt(low_dot*high_dot);
+	double con_l = low_high_norm*low_high_norm / area;
+	double score = c1*a_stddev[0] + c2*con_l + c3*b_mean[0];
 	return score;
 }
