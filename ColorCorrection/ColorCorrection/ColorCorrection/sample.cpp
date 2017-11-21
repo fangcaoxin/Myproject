b6ff319db33f8683b5ba4b8d11b4548a771f363f
qiaoxiaorui <qiaoxiaorui@morphoinc.com>
2017-11-21 19:06:02 +0900

    add PCQI

--- ColorCorrection/ColorCorrection/ColorCorrection/sample.cpp
+++ ColorCorrection/ColorCorrection/ColorCorrection/sample.cpp
@@ -36,6 +36,48 @@ static void applyCLAHE(Mat& img) {
 
 }
 
+static double PCQI(Mat& img_1, Mat& img_2) {
+	
+	int height = img_1.rows;
+	int width = img_1.cols;
+	img_1.convertTo(img_1, CV_32FC1);
+	img_2.convertTo(img_2, CV_32FC1);
+	int patch_num = 0;
+	
+	double pcqi = 0.;
+	Rect image_rect(0, 0, width, height);
+	for (int i = 0; i < height-height / 4; i = i + height / 4) {
+		for (int j = 0; j < width-width / 4; j = j + width / 4) {
+			patch_num++;
+			Rect rect(i, j, width / 4, height / 4);
+			Rect valid_rect = rect& image_rect;
+			Mat patch_x = img_1(valid_rect);
+			Mat patch_y = img_2(valid_rect);
+			int N = sqrt(double(valid_rect.width*valid_rect.height));
+			double v1 = 1. / N;
+
+			Scalar mean_patch = mean(patch_x);
+			Scalar mean_patch_y = mean(patch_y);
+
+			double c1_x = N*mean_patch[0];
+			double c1_y = N*mean_patch_y[0];
+	
+			Mat c2_y_r = patch_y -mean_patch_y[0];
+			
+			Mat v2 = (patch_x - mean_patch[0]) / norm(patch_x - mean_patch[0]);
+			
+			double q_s = c2_y_r.dot(v2) / (norm(c2_y_r)*norm(v2));
+			cout << c2_y_r.dot(v2) << endl;
+			double c2_x = norm(patch_x - mean_patch[0]);
+			double c2_y = patch_y.dot(v2);
+			double q_c = 4*atan(abs(c2_y/c2_x))/CV_PI; //0~2
+			double q_i = exp(-(c1_x - c1_y) / (N * 255));
+			pcqi += q_c*q_s*q_i;
+		}
+	}
+	return pcqi /patch_num;
+}
+
 
 
 int main(int argc, char** argv) {
@@ -66,16 +108,24 @@ int main(int argc, char** argv) {
 #endif //LIST
 
 	
-	string filename = "..//..//image//color_chart.png";
-	//string filename1 = "..//..//image//img_170724_02j_5.jpg";
-	//Mat image = imread(filename);
-	Mat image = imread(filename);
+	string filename = "..//..//image//out_4.jpg";
+	string filename1 = "..//..//image//res_1.jpg";
+	Mat image = imread(filename,0);
+	Mat image1 = imread(filename1,0);
 	/*Ptr<StereoBM> s_BM= StereoBM::create(16, 21);
 	Mat disparity;
 	s_BM->compute(image, image1, disparity);
 	disparity.convertTo(disparity, CV_32FC1);
 	normalize(disparity, disparity, 0, 0.8, NORM_MINMAX);*/
-	
+//#define PSNREVALUATION
+#ifdef PSNREVALUATION
+	double psnr_score = PSNR(image, image1);
+	cout << "psnr_score " << psnr_score << endl;
+#endif //PSNREVALUATION
+#define PCQI_C
+#ifdef PCQI_C
+	double score_PCQI = PCQI(image, image1);
+#endif //PCQI_C
 	////Mat diff = abs(image - image1);
 	//imshow("dis", disparity);
 	Mat res,res1,res2;
@@ -121,7 +171,7 @@ int main(int argc, char** argv) {
 	applyCLAHE(image);
 #endif //HISTOGRAM
 	//opticalModelCorrect(image, res2);
-	dehaze(image, res2);
+	//dehaze(image, res2);
 	//dehazeMY(res, res1);
 	//enhance(image, res2);
 	//dehazeDC(image, res);
@@ -132,14 +182,14 @@ int main(int argc, char** argv) {
 	//imwrite("res.jpg", res1);
 	//imwrite("res_1102.jpg", res2);
 	
-	double s2 = evaluationScore_UCIQUE(res2);
-	cout << "s2 " << s2 << endl;
-	imshow("res", res2);
-	imwrite("res_udcp_color.jpg", res2);
+	//double s2 = evaluationScore_UCIQUE(res2);
+	//cout << "s2 " << s2 << endl;
+	//imshow("res", res2);
+	////imwrite("res_udcp_color.jpg", res2);
 	/*cout << "blue " << res1.at<Vec3b>(319, 126) << endl;
 	cout << "green " << res1.at<Vec3b>(311, 191) << endl;
 	cout << "red " << res1.at<Vec3b>(314, 238) << endl;*/
-	imshow("histogram", image);
+	//imshow("histogram", image);
 	waitKey(0);
 
 }
