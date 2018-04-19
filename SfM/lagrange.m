046812d78caef2cccef7bf6149f1714ae930acca
xiaoruiqiao <xiaoruiqiao@gmail.com>
2018-04-19 22:25:25 +0900

    larange

--- SfM/lagrange.m
+++ SfM/lagrange.m
@@ -1,6 +1,6 @@
 function g=lagrange(U,g0)
-	gg0=[g0;0;0;0;0;0;0];%ggï¿½Ìï¿½ï¿½ï¿½ï¿½ï¿½ï¿½D
-	f=@(gg)Ug(gg,U);%ggï¿½ÍCgï¿½ÉCalphaï¿½Cbetaï¿½ï¿½ï¿½Ç‰ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ÌD
+	gg0=[g0;0;0;0;0;0;0];%gg?½Ìï¿½?½?½?½?½?½D
+	f=@(gg)Ug(gg,U);%gg?½ÍCg?½ÉCalpha?½Cbeta?½?½?½Ç‰ï¿½?½?½?½?½?½?½ÌD
 %	[gg,fval,info]=fsolve(f,gg0,optimset("TolFun",3e-16,"TolX",3e-16,"MaxIter",1e20));
    % options=optimoptions('fsolve','Display','off','TolFun',1e-10,'TolX',1e-10,'MaxIter',1e20);
 	%[gg,fval,info]=fsolve(f,gg0,options);
@@ -14,12 +14,12 @@ function g=lagrange(U,g0)
 	g(19,:)=[];
   g(19,:)=[];
   g(19,:)=[];
-	g(19,:)=[];%ï¿½ï¿½ï¿½ï¿½ï¿½Å‚ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½Æ‚ï¿½ï¿½É‚È‚ï¿½
+	g(19,:)=[];%?½?½?½?½?½Å‚ï¿½?½?½?½?½?½Æ‚ï¿½?½É‚È‚ï¿½
 end
 
 
 function Ug_val=Ug(gg,U)
-%ggï¿½ÍCgï¿½ÉCalphaï¿½Cbetaï¿½ï¿½ï¿½Ç‰ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ÌD
+%gg?½ÍCg?½ÉCalpha?½Cbeta?½?½?½Ç‰ï¿½?½?½?½?½?½?½ÌD
 	g=gg;
     g(19,:)=[];
     g(19,:)=[];
@@ -43,7 +43,7 @@ function Ug_val=Ug(gg,U)
 	Ug_val(13)= UU(13,:)*g+gg(20)*gg(13)-gg(11)*gg(24)+gg(12)*gg(23)+gg(10)*gg(21);
 	Ug_val(14)= UU(14,:)*g+gg(20)*gg(14)-gg(12)*gg(22)+gg(10)*gg(24)+gg(11)*gg(21);
 	Ug_val(15)= UU(15,:)*g+gg(20)*gg(15)-gg(10)*gg(23)+gg(11)*gg(22)+gg(12)*gg(21);
-  Ug_val(16)= UU(16,:)*g-gg(22);
+    Ug_val(16)= UU(16,:)*g-gg(22);
 	Ug_val(17)= UU(17,:)*g-gg(23);
 	Ug_val(18)= UU(18,:)*g-gg(24);
 	Ug_val(19)= (gg(10)^2+gg(11)^2+gg(12)^2-1)/2;
