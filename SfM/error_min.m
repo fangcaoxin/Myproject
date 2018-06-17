b5d29ae2e314fbdf62e1f16eece7e218f84cbba2
xiaoruiqiao <xiaoruiqiao@gmail.com>
2018-06-18 08:59:19 +0900

    calibrate

--- SfM/error_min.m
+++ SfM/error_min.m
@@ -1,17 +1,44 @@
-function [r_out, x_s] = ray_in_out_pixel(uv,d, cali)
-load parameter.mat;
-if cali==1
-    n3 = n1;
+function res =  error_min(init, x,x_w, K, c, Ra, ra)
+fun = @(gg)fun1(gg, x, K, c, Ra, ra) - x_w;
+lb = [-1 -1 -1 -1 -1 -1 -500 -500 0 0];
+ub = [1 1 1 1 1 1 500 500 800 45];
+options=optimoptions('lsqnonlin', 'Display','iter','FunctionTolerance',1e-10);
+res = lsqnonlin(fun, init,lb, ub, options);
+problem = createOptimProblem('lsqnonlin','x0',init,'objective',fun,...
+    'lb',lb,'ub',ub);
+ms = MultiStart;
+[xx,f] = run(ms, problem, 30);
+f
+res = xx;
 end
-u = uv(1) - hcx; % coord on image sensor
-v = uv(2) - hcy;
+
+function val = fun1(gg, x, K, c, Ra, ra)
+r1 = [gg(1) gg(2) gg(3)];
+r2 = [gg(4) gg(5) gg(6)];
+r1 = r1/norm(r1);
+r2 = r2/norm(r2);
+r3 = cross(r1,r2);
+Rot = [r1;r2;r3];
+ts = [gg(7); gg(8);gg(9)];
+d = gg(10);
+hcx = K(3,1);
+hcy = K(3,2);
+fx = K(1,1);
+fy = K(2,2);
+n1 = c(1);
+n2 = c(2);
+n3 = c(3);
+R = Ra;
+r = ra;
+u = x(1) - hcx; % coord on image sensor
+v = x(2) - hcy;
 r_in=[u, v*fx/fy, fx]; % ray in air
 r_in = r_in/norm(r_in); % normalize
-t_0 = (-r_in(3)*d + sqrt(r_in(3)*r_in(3)*d*d-(r_in(2)*r_in(2)+r_in(3)*r_in(3))*(d*d-r*r)))/(r_in(2)*r_in(2)+r_in(3)*r_in(3));
-   if(d+r_in(3)*t_0 < 0)
+t_0 = (-r_in(3)*d + sqrt(r_in(3)*r_in(3)*d*d-(r_in(2)*r_in(2)+r_in(3)*...
+    r_in(3))*(d*d-r*r)))/(r_in(2)*r_in(2)+r_in(3)*r_in(3));
+ if(d+r_in(3)*t_0 < 0)
       t_0 = (-r_in(3)*d - sqrt(r_in(3)*r_in(3)*d*d-(r_in(2)*r_in(2)+r_in(3)*r_in(3))*(d*d-r*r)))/(r_in(2)*r_in(2)+r_in(3)*r_in(3));
-   end
-
+ end
 p1 = [r_in(1)*t_0 r_in(2)*t_0 r_in(3)*t_0+d]; % point at glass and air
 N = [0 p1(2) p1(3)]; % normal between air and glass
 N_norm = N/norm(N);
@@ -33,4 +60,9 @@ c2 = norm(r_glass_norm.*N1_norm);
 r_out = n2/n3*r_glass_norm - (n2/n3*c2-sqrt(1- n2*n2/(n3*n3)*s2*s2))*N1_norm;
 r_out = r_out/norm(r_out);
 x_s = p2;
+xs_w = Rot'*(x_s' - ts);
+r_out_w = Rot'*r_out';
+lamda = -xs_w(3)/r_out_w(3);
+val(1) = xs_w(1) + lamda*r_out_w(1);
+val(2) = xs_w(2) + lamda*r_out_w(2);
 end
--- SfM/lagrange.m
+++ SfM/lagrange.m
@@ -1,56 +1,33 @@
-function g=lagrange(U,g0)
-	gg0=[g0;0;0;0;0;0;0];%gg?ｽﾌ擾ｿｽ?ｽ?ｽ?ｽ?ｽ?ｽD
-	f=@(gg)Ug(gg,U);%gg?ｽﾍ，g?ｽﾉ，alpha?ｽCbeta?ｽ?ｽ?ｽﾇ会ｿｽ?ｽ?ｽ?ｽ?ｽ?ｽ?ｽﾌ．
-%	[gg,fval,info]=fsolve(f,gg0,optimset("TolFun",3e-16,"TolX",3e-16,"MaxIter",1e20));
-   % options=optimoptions('fsolve','Display','off','TolFun',1e-10,'TolX',1e-10,'MaxIter',1e20);
-	%[gg,fval,info]=fsolve(f,gg0,options);
-  [gg,fval,info]=fsolve(f,gg0);
-    
-%info
-	g=gg;
-%fval
-	g(19,:)=[];
-  g(19,:)=[];
-	g(19,:)=[];
-  g(19,:)=[];
-  g(19,:)=[];
-	g(19,:)=[];%?ｽ?ｽ?ｽ?ｽ?ｽﾅゑｿｽ?ｽ?ｽ?ｽ?ｽ?ｽﾆゑｿｽ?ｽﾉなゑｿｽ
+function g = lagrange(U,C,g0)
+gg0 = [g0; 0; 0;0];
+f=@(gg)Ug(gg,U,C);
+options=optimoptions('fsolve','Display','off','TolFun',1e-10,'TolX',1e-10,'MaxIter',1e20);
+[gg,fval,info]=fsolve(f,gg0,options);
+g =gg;
+g(10,:)=[];
+g(10,:)=[];
+g(10,:)=[];
 end
 
-
-function Ug_val=Ug(gg,U)
-%gg?ｽﾍ，g?ｽﾉ，alpha?ｽCbeta?ｽ?ｽ?ｽﾇ会ｿｽ?ｽ?ｽ?ｽ?ｽ?ｽ?ｽﾌ．
-	g=gg;
-    g(19,:)=[];
-    g(19,:)=[];
-	g(19,:)=[];
-	g(19,:)=[];
-    g(19,:)=[];
-    g(19,:)=[];
-    UU=U'*U;
-	Ug_val(1)= UU(1,:)*g;
-	Ug_val(2)= UU(2,:)*g;
-	Ug_val(3)= UU(3,:)*g;
-	Ug_val(4)= UU(4,:)*g;
-	Ug_val(5)= UU(5,:)*g;
-	Ug_val(6)= UU(6,:)*g;
-	Ug_val(7)= UU(7,:)*g;
-	Ug_val(8)= UU(8,:)*g;
-	Ug_val(9)= UU(9,:)*g;
-	Ug_val(10)= UU(10,:)*g+gg(19)*gg(10)-gg(15)*gg(23)+gg(14)*gg(24)+gg(13)*gg(21);
-	Ug_val(11)= UU(11,:)*g+gg(19)*gg(11)-gg(13)*gg(24)+gg(15)*gg(22)+gg(14)*gg(21);
-	Ug_val(12)= UU(12,:)*g+gg(19)*gg(12)-gg(14)*gg(22)+gg(13)*gg(23)+gg(15)*gg(21);
-	Ug_val(13)= UU(13,:)*g+gg(20)*gg(13)-gg(11)*gg(24)+gg(12)*gg(23)+gg(10)*gg(21);
-	Ug_val(14)= UU(14,:)*g+gg(20)*gg(14)-gg(12)*gg(22)+gg(10)*gg(24)+gg(11)*gg(21);
-	Ug_val(15)= UU(15,:)*g+gg(20)*gg(15)-gg(10)*gg(23)+gg(11)*gg(22)+gg(12)*gg(21);
-    Ug_val(16)= UU(16,:)*g-gg(22);
-	Ug_val(17)= UU(17,:)*g-gg(23);
-	Ug_val(18)= UU(18,:)*g-gg(24);
-	Ug_val(19)= (gg(10)^2+gg(11)^2+gg(12)^2-1)/2;
-	Ug_val(20)= (gg(13)^2+gg(14)^2+gg(15)^2-1)/2;
-	Ug_val(21)= gg(10)*gg(13)+gg(11)*gg(14)+gg(12)*gg(15);
-  Ug_val(22)= gg(11)*gg(15)-gg(12)*gg(14)-gg(16);
-  Ug_val(23)= gg(12)*gg(13)-gg(10)*gg(15)-gg(17);
-  Ug_val(24)= gg(10)*gg(14)-gg(11)*gg(13)-gg(18);
-	
+function Ug_val = Ug(gg, U,C)
+ g = gg;
+ g(10,:)=[];
+ g(10,:)=[];
+ g(10,:)=[];
+ UU = U'*U;
+ A = U';
+ C1=-A(1,:)*C;
+ C2 =-C'*U;
+ Ug_val(1) = UU(1,:)*g -A(1,:)*C + C2(1)+ gg(10)*gg(1) + gg(12)*gg(4);
+ Ug_val(2) = UU(2,:)*g -A(2,:)*C + C2(2)+ gg(10)*gg(2) + gg(12)*gg(5);
+ Ug_val(3) = UU(3,:)*g -A(3,:)*C + C2(3)+ gg(10)*gg(3) + gg(12)*gg(6);
+ Ug_val(4) = UU(4,:)*g- A(4,:)*C + C2(4)+ gg(11)*gg(4) + gg(12)*gg(1);
+ Ug_val(5) = UU(5,:)*g -A(5,:)*C + C2(5)+ gg(11)*gg(5) + gg(12)*gg(2);
+ Ug_val(6) = UU(6,:)*g -A(6,:)*C + C2(6)+ gg(11)*gg(6) + gg(12)*gg(3);
+ Ug_val(7) = UU(7,:)*g -A(7,:)*C + C2(7);
+ Ug_val(8) = UU(8,:)*g -A(8,:)*C + C2(8);
+ Ug_val(9) = UU(9,:)*g -A(9,:)*C + C2(9);
+ Ug_val(10) = (gg(1)^2 + gg(2)^2 + gg(3)^2 -1)/2;
+ Ug_val(11) = (gg(4)^2 + gg(5)^2 + gg(6)^2 -1)/2;
+ Ug_val(12) = gg(1)*gg(4)+gg(2)*gg(5)+gg(3)*gg(6);
 end
