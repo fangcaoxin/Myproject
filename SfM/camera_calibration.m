823940b09b08067577861c2889b0592036374afa
qiaoxiaorui <qiaoxiaorui@morphoinc.com>
2018-06-18 10:28:09 +0900

    commit before update

--- SfM/camera_calibration.m
+++ SfM/camera_calibration.m
@@ -12,7 +12,7 @@ r_in = r_in/norm(r_in);
 
 t_0 = (-r_in(3)*cc(3)-r_in(2)*cc(2) + sqrt((r_in(2)*cc(2)+ r_in(3)*cc(3))^2-(r_in(2)*r_in(2)+r_in(3)*r_in(3))*(cc(2)*cc(2)+cc(3)*cc(3)-r*r)))/(r_in(2)*r_in(2)+r_in(3)*r_in(3));
 p1 = [cc(1)+r_in(1)*t_0 cc(2)+r_in(2)*t_0 cc(3)+r_in(3)*t_0]; % point at glass and air
-N = [0 p1(2) p1(3)]; % normal between air and glass
+N = [0; p1(2); p1(3)]; % normal between air and glass
 N_norm = N/norm(N);
 s1 = norm(cross(r_in, N_norm)); % sin(theta_1)
 c1 =norm( r_in.*N_norm); % cos(theta_1)
@@ -23,7 +23,7 @@ t_1 =( -(p1(2)*r_glass_norm(2) +p1(3)*r_glass_norm(3)) + sqrt((p1(2)*r_glass_nor
       -(r_glass_norm(2)*r_glass_norm(2)+r_glass_norm(3)*r_glass_norm(3))*(p1(2)*p1(2) + p1(3)*p1(3)-R*R)))/(r_glass_norm(2)*r_glass_norm(2)+r_glass_norm(3)*r_glass_norm(3));
 
 p2 = [p1(1)+t_1*r_glass_norm(1) p1(2)+t_1*r_glass_norm(2) p1(3)+t_1*r_glass_norm(3)]; % point at glass and water
-N1 = [0 p2(2) p2(3)]; % normal between glass and water
+N1 = [0; p2(2); p2(3)]; % normal between glass and water
 N1_norm = N1/norm(N1);
 s2 = norm(cross(r_glass_norm,N1_norm));
 c2 = norm(r_glass_norm.*N1_norm);
--- SfM/points_reconstruction.m
+++ SfM/points_reconstruction.m
@@ -2,18 +2,19 @@ function points_3d = points_reconstruction
 load ray1_select.mat
 load ray2_select.mat
 load R_t_est.mat
-load d_vector.mat
 load effctive_num.mat 
 load camera_motion.mat
-R_est = Rotate;
-t_est = translation;
+load cross_point_effctive.mat
+
+%R_est = Rotate;
+%t_est = translation;
 point_num = effctive_point;
 points_3d = zeros(point_num,3);
 for i = 1: point_num
     r1= ray1_out_select(i,:)';
     r2 =ray2_out_select(i,:)';
-    d2 = d2_mat(i,:);
-    d1 = d1_mat(i,:);
+    d2 = cross_point2_effctive(i,:);
+    d1 = cross_point1_effctive(i,:);
     r2w = R_est'*r2;
     d2w = R_est'*d2';
     dd =t_est + d2w;
