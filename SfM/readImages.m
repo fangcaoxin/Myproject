18b936d88ccf09282d64953a75405dc63d673d34
qiaoxiaorui <qiaoxiaorui@morphoinc.com>
2018-07-12 18:11:49 +0900

    add readImages function

---
+++ SfM/readImages.m
@@ -0,0 +1,9 @@
+function images = readImages()
+    imageDir = fullfile('', '', '',);
+    imds = imageDatastore(imageDir);
+    images = cell(1, numel(imds.Files));
+    for i = 1: numel(imds.Files)
+       I = readimage(imds, i);
+       images{i} = rgb2gray(I);
+    end
+end
