db172ef60f88dfa6b77e4bfa1d259829478c4b7f
xiaoruiqiao <xiaoruiqiao@gmail.com>
2018-09-04 21:44:06 +0900

    simulation

--- SfM/bundle_adjustion.m
+++ SfM/bundle_adjustion.m
@@ -1,7 +1,10 @@
 %load imagePoints.mat
 % load cube_points.mat
 load imagePointMatrix.mat
+load basePoint.mat
 views = [1 2 3 4 5];
-
-[xw_est, Rot, trans] = sfm_multi_view_simulation(imagePointMatrix, views);
+imagepoints = zeros(size(basePoint,1), size(basePoint,2), size(imagePointMatrix,3)+1);
+imagepoints(:,:,1) = basePoint;
+imagepoints(:,:,2:end) = imagePointMatrix;
+[xw_est, Rot, trans] = sfm_multi_view_simulation(imagepoints, views);
 scatter3(xw_est(:,1), xw_est(:,2), xw_est(:,3));
